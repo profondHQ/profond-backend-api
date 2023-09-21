bdab243a05025a270e163e01d60f317affe36236
Irfi <irfi@paras.id>
2023-09-21 19:48:53 +0700

    refactor: remove old project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,18 @@
-import 'dotenv/config';
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ProjectModule } from './project/project.module';
-import { CoinModule } from './coin/coin.module';
-import { CollectionModule } from './collection/collection.module';
+import "dotenv/config";
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { CoinModule } from "./coin/coin.module";
+import { CollectionModule } from "./collection/collection.module";
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL),
-    ProjectModule,
     CoinModule,
-    CollectionModule,
+    CollectionModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
