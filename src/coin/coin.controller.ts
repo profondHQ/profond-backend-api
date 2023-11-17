8c1ce3386d9153f21bff850430e0218f26c359b2
Irfi M <irfi@localcitizen>
2023-11-17 20:57:11 +0700

    fix: comma

--- src/coin/coin.controller.ts
+++ src/coin/coin.controller.ts
@@ -3,12 +3,12 @@ import { CoinService } from "./coin.service";
 
 @Controller("coins")
 export class CoinController {
-  constructor(private readonly coinService: CoinService) { }
+  constructor(private readonly coinService: CoinService) {}
 
   @Get()
   findAll(
     @Query("minter_address") minter_address?: string,
-    @Query("chain") chain?: string
+    @Query("chain") chain?: string,
     @Query("is_on_sale") is_on_sale?: string
   ) {
     return this.coinService.findAll(minter_address, chain, is_on_sale);
