09c4ef5bac19140ea586310e6eee4d1630902663
Irfianto <irfi@paras.id>
2023-12-18 21:17:32 +0700

    test: fix e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
 
-describe('AppController (e2e)', () => {
+describe("AppController (e2e)", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    await app.init();
+    return await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll((done) => {
+    app.close();
+    done();
+  });
+
+  it("Server works", async () => {
+    const result = await request(app.getHttpServer()).get("/");
+
+    expect(result.statusCode).toEqual(200);
+    expect(result.text).toEqual('{"status":1}');
   });
 });
